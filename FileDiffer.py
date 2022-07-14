8655cee0d2c9f50114bc50e69ad4000a17763801
Jay Xie <33448862@student.murdoch.edu.au>
2022-07-15 00:58:57 +0800

    fixed logical errors

--- FileDiffer.py
+++ FileDiffer.py
@@ -1,19 +1,21 @@
+from asyncio.windows_events import NULL
 import json
 import difflib
+from unittest.util import _MAX_LENGTH
 
 class Added:
-    def __init__(self, value, position):
+    def __init__(self, value, atPosition):
         self.value = value
-        self.position = position
+        self.atPosition = atPosition
 
     def toJson(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
             sort_keys=True, indent=4)
 
 class Deleted:
-    def __init__(self, value, position):
+    def __init__(self, value, atPosition):
         self.value = value
-        self.position = position
+        self.atPosition = atPosition
 
     def toJson(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
@@ -22,7 +24,7 @@ class Deleted:
 class Line:
     def __init__(self):
         self.text = ""
-        self.number = 0
+        self.lineNumber = 0
         self.deleted = list()
         self.added = list()
 
@@ -44,38 +46,45 @@ def Diff(fileName1, fileName2):
 
     d = difflib.Differ()
 
+    # open files for reading
     f1 = open(fileName1, "r")
     f2 = open(fileName2, "r")
 
-    t1 = f1.read()
-    t2 = f2.read()
-
-    lineNumber = 0
-    modPosition = 0
+    # read and split files by line
+    t1 = f1.read().split("\n")
+    t2 = f2.read().split("\n")
 
     result = DiffResult()
-    result.lines.append(Line())
 
-    # for every character in the diff result
-    for i, diff in enumerate(list(d.compare(t1, t2))):
-
-        # add to line string
-        result.lines[lineNumber].text += diff[2]
-
-
-        # add modification info
-        if diff[0] == "+":
-            result.lines[lineNumber].added.append(Added(diff[2], modPosition))
-        elif diff[0] == "-":
-            result.lines[lineNumber].deleted.append(Deleted(diff[2], modPosition))
-
-        modPosition += 1
+    maxLength = max(len(t1), len(t2))
     
-        # at the end of line
-        if diff[2] == "\n":
-            result.lines.append(Line())
-            result.lines[lineNumber].number = lineNumber+1
-            lineNumber += 1
-            modPosition = 0
+    if len(t1) < maxLength:
+        t1.extend([""] * (maxLength - len(t1)))
+
+    if len(t2) < maxLength:
+        t2.extend([""] * (maxLength - len(t2)))
+
+    for i in range(max(len(t1), len(t2))):
+
+        result.lines.append(Line())
+
+        modPosition = 0
+        
+        # for every character in the diff result
+        for diff in list(d.compare(t1[i], t2[i])):
+
+            line = result.lines[i]
+               
+            line.lineNumber = i + 1
+            line.text += diff[2]
+
+            # add modification info
+            if diff[0] == "+":
+                line.added.append(Added(diff[2], modPosition))
+            elif diff[0] == "-":
+                line.deleted.append(Deleted(diff[2], modPosition))
+
+            modPosition += 1
 
     return result.toJson()
+
